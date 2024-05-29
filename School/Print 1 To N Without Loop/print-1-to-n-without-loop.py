5e98ab741f1629feb74b62a242d4db59056b24eb
Mrigakshi Chib <132836653+mrigakshichib@users.noreply.github.com>
2024-05-29 16:56:00 +0530

    Added Solution - GfG to GitHub

---
+++ School/Print 1 To N Without Loop/print-1-to-n-without-loop.py
@@ -0,0 +1,38 @@
+#User function Template for python3
+
+class Solution:    
+    #Complete this function
+    def printNos(self,N):
+        #Your code here
+        for i in range(1,N+1):
+            print(i,end=" ")
+            
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+
+
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        N=int(input())
+        
+        ob=Solution()
+        
+        ob.printNos(N)
+        print()
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
