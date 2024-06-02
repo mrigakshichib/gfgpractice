11021c3c75df08f1d55814ef1987f9b5b54e1d82
Mrigakshi Chib <132836653+mrigakshichib@users.noreply.github.com>
2024-06-02 18:51:10 +0530

    Updated Solution - GfG to GitHub

--- Easy/Bubble Sort/bubble-sort.py
+++ Easy/Bubble Sort/bubble-sort.py
@@ -1,15 +1,15 @@
 #User function Template for python3
-#in Bubble sort, what happens is that adjacent elements are comapred till the time we get our sorted array
+
 class Solution:
     #Function to sort the array using bubble sort algorithm.
     def bubbleSort(self,arr, n):
         # code here
         n=len(arr)
-        for i in range(n-1):#this loop is for the number of passes that will put the largest number at the end of it
-            for j in range(n-i-1):#this loop iterates through the array ,here we put -1 as we shouldnt compare the largest element which has already been put at the right position
+        for i in range(n-1):
+            for j in range(n-i-1):
                 if arr[j+1]<arr[j]:
                     arr[j],arr[j+1]=arr[j+1],arr[j]
-        return arr        
+        return arr            
 
 
 #{ 
