5fd4dc5681b0c8292cde46a60cbe96fd356770a2
Mrigakshi Chib <132836653+mrigakshichib@users.noreply.github.com>
2024-07-03 16:18:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Remove all occurences of duplicates in a linked list/remove-all-occurences-of-duplicates-in-a-linked-list.py
@@ -0,0 +1,130 @@
+#User function Template for python3
+
+"""
+# Linked list Node class
+
+    class Node :
+        def __init__(self, val):
+            self.data = val
+            self.next = None
+
+"""
+
+class Solution:
+    def removeAllDuplicates(self, head):
+        #code here
+        dummy = Node(0)
+        dummy.next = head
+        
+        # prev is the last node before the sublist of duplicates
+        prev = dummy
+        current = head
+        
+        while current:
+            # Check if current node has duplicates
+            has_duplicates = False
+            while current.next and current.data == current.next.data:
+                has_duplicates = True
+                current = current.next
+            
+            if has_duplicates:
+                # Skip all nodes with current value
+                prev.next = current.next
+            else:
+                # Move prev to current
+                prev = prev.next
+            
+            # Move current forward
+            current = current.next
+        
+        return dummy.next
+
+# Helper function to print the list
+def print_list(head):
+    while head:
+        print(head.data, end=" ")
+        head = head.next
+    print()
+
+# Helper function to create a linked list from a list of values
+def create_linked_list(arr):
+    if not arr:
+        return None
+    head = Node(arr[0])
+    current = head
+    for value in arr[1:]:
+        current.next = Node(value)
+        current = current.next
+    return head
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+
+@atexit.register
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
+# Node Class
+class Node:
+
+    def __init__(self, data):  # data -> value stored in node
+        self.data = data
+        self.next = None
+
+
+# Linked List Class
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    # creates a new node with given value and appends it at the end of the linked list
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            return
+        self.tail.next = new_node
+        self.tail = new_node
+
+    # prints the elements of linked list starting with head
+    def printList(self, head):
+        if head is None:
+            print(' ')
+            return
+        curr_node = head
+        while curr_node:
+            print(curr_node.data, end=" ")
+            curr_node = curr_node.next
+        print(' ')
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for cases in range(t):
+        N = int(input())
+        a = LinkedList()  # create a new linked list 'a'.
+        nodes = list(map(int, input().strip().split()))
+        for x in nodes:
+            a.append(x)
+        ob = Solution()
+        head = ob.removeAllDuplicates(a.head)
+        a.printList(head)
+
+# } Driver Code Ends
