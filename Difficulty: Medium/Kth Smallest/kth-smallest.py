063edc1946691c6b7981850b6019711ce6b6d3d8
Mrigakshi Chib <132836653+mrigakshichib@users.noreply.github.com>
2024-08-22 20:32:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Smallest/kth-smallest.py
+++ Difficulty: Medium/Kth Smallest/kth-smallest.py
@@ -1,11 +1,32 @@
 #User function Template for python3
-import numpy as np
+#import numpy as np
 class Solution:
+    def kthSmallest(self, arr, k):
+        return self.quickselect(arr, 0, len(arr) - 1, k)
 
-    def kthSmallest(self, arr,k):
-        sorted_arr = np.sort(arr)
-        return sorted_arr[k-1]
-        
+    def partition(self, arr, low, high):
+        pivot = arr[high]
+        i = low - 1
+
+        for j in range(low, high):
+            if arr[j] <= pivot:
+                i += 1
+                arr[i], arr[j] = arr[j], arr[i]
+                
+        arr[i + 1], arr[high] = arr[high], arr[i + 1]
+        return i + 1
+
+    def quickselect(self, arr, low, high, k):
+        if low <= high:
+            pi = self.partition(arr, low, high)
+
+            if pi == k - 1:
+                return arr[pi]
+            elif pi > k - 1:
+                return self.quickselect(arr, low, pi - 1, k)
+            else:
+                return self.quickselect(arr, pi + 1, high, k)
+        return -1
         
 
 
