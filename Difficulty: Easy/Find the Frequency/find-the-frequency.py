11a46fb79bc542445013f739ff0f7997382b8a45
Mrigakshi Chib <132836653+mrigakshichib@users.noreply.github.com>
2024-08-22 22:04:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find the Frequency/find-the-frequency.py
@@ -0,0 +1,30 @@
+#User function Template for python3
+
+"""
+You're given an array (arr) of length n
+Return the frequency of element x in the given array
+"""
+def findFrequency (arr, n, x):
+    # Your Code Here
+    count = 0
+    for i in range(len(arr)):
+        if arr[i] == x:
+            count+=1
+    return count
+        
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+t = int (input ())
+for tc in range (t):
+    n = int (input ())
+    arr = list (map (int, input ().split ()))
+    x = int (input ())
+    print (findFrequency (arr, n, x))
+    
+
+# } Driver Code Ends
